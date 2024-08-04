3cef77ddda1c9c91a4f6f5b61f389e4dd546ddca
caroc24 <carolinacamarda@gmail.com>
2024-08-04 13:59:35 -0400

    uploading love calculator

---
+++ loveCalculator.py
@@ -0,0 +1,30 @@
+print("The Love Calculator is calculating your score...")
+name1 = input() # What is your name?
+name2 = input() # What is their name?
+# 🚨 Don't change the code above 👆
+# Write your code below this line 👇
+
+name1 = name1.lower()
+name2 = name2.lower()
+
+t = name1.count("t") + name2.count("t")
+r = name1.count("r") + name2.count("r")
+u = name1.count("u") + name2.count("u")
+e = name1.count("e") + name2.count("e")
+
+l = name1.count("l") + name2.count("l")
+o = name1.count("o") + name2.count("o")
+v = name1.count("v") + name2.count("v")
+#e = name1.count("e") + name2.count("e")
+
+totalTrue = t + r + u + e
+totalLove = l + o + v + e
+
+score = (totalTrue * 10) + totalLove
+
+if score < 10 or score > 90:
+  print(f"Your score is {score}, you go together like coke and mentos.")
+elif score > 40 and score < 50:
+  print(f"Your score is {score}, you are alright together.")
+else:
+  print(f"Your score is {score}.")
