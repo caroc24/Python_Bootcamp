9eee8dbef251aa377b079b62a63eb01dbb56070f
caroc24 <carolinacamarda@gmail.com>
2024-08-18 14:13:04 -0400

    moving if statements around

--- coffeeMachine.py
+++ coffeeMachine.py
@@ -42,6 +42,7 @@ def report():
 
 
 def check_resources(drink):
+    """Returns true when order can be made, and false when it can't."""
     req_water = MENU[drink]['ingredients']['water']
     req_milk = MENU[drink]['ingredients']['milk']
     req_coffee = water = MENU[drink]['ingredients']['coffee']
@@ -62,31 +63,36 @@ def check_resources(drink):
     else:
         return True
 
-def process_coins():
-    print("Please insert coins.\n")
-    quarters = int(input("How many quarters?: "))
-    dimes = int(input("How many dimes?: "))
-    nickels = int(input("How many nickels?: "))
-    pennies = int(input("How many pennies?: "))
 
-    total_money = quarters * 0.25 + dimes * 0.1 + nickels * 0.05 + pennies * 0.01
+def process_coins():
+    """Returns the total calculated from coins inserted."""
+    print("Please insert coins.\n")
+    quarters = int(input("How many quarters?: ")) * 0.25
+    dimes = int(input("How many dimes?: ")) * 0.1
+    nickels = int(input("How many nickels?: ")) * 0.05
+    pennies = int(input("How many pennies?: ")) * 0.01
+
+    total_money = quarters + dimes + nickels + pennies
     return total_money
+
 
 def coffee_machine():
     machine_is_on = True
     while machine_is_on:
         coffee_order = input("What would you like? (espresso/latte/cappuccino) ")
-        if coffee_order == 'espresso':
+        if coffee_order == 'off':
+            machine_is_on = False
+        elif coffee_order == 'report':
+            report()
+        else:
+            coffee_order == 'espresso'
             if check_resources(coffee_order):
                 espresso_cost = MENU[coffee_order]['ingredients']['cost']
                 money_inserted = process_coins()
                 if money_inserted > espresso_cost:
                     change = money_inserted - espresso_cost
-                    #resources["money"].append(1.50)
-                    #print( f"Here is {change} dollars in change.”)
-        elif coffee_order == 'report':
-            report()
-        elif coffee_order == 'off':
-            machine_is_on = False
+                    resources["money"].append(1.50)
+                    print(f"Here is {change} dollars in change.")
+
 
 coffee_machine()
