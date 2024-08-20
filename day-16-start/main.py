e2fb707737d10800108470858c773bb679716024
caroc24 <carolinacamarda@gmail.com>
2024-08-20 18:37:00 -0400

    imported pretty table

--- day-16-start/main.py
+++ day-16-start/main.py
@@ -1,11 +1,14 @@
-from turtle import Turtle, Screen
-import prettytable
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("chartreuse")
-timmy.forward(100)
+# from turtle import Turtle, Screen
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("chartreuse")
+# timmy.forward(100)
+
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+table = PrettyTable
