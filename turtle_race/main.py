ee8c810149c69897868e0f92ac4fe8aef52e841d
caroc24 <carolinacamarda@gmail.com>
2024-09-18 13:16:02 -0400

    set up for loop for turtle creation

--- turtle_race/main.py
+++ turtle_race/main.py
@@ -3,8 +3,14 @@ from turtle import Turtle, Screen
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet.", prompt="Which turtle will win the race? Enter a color:")
-print(user_bet)
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_pos = [-75, -45, -15, 15, 45, 75]
 
-tim = Turtle()
+for i in range(0,6):
+    tim = Turtle(shape="turtle")
+    tim.color(colors[i])
+    tim.penup()
+    tim.goto(x=-230, y=y_pos[i])
+
 
 screen.exitonclick()
