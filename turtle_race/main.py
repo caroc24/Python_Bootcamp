4e7a59039171979c8e10da5bdd74b0c4d1878af1
caroc24 <carolinacamarda@gmail.com>
2024-09-18 12:57:46 -0400

    initial set up

---
+++ turtle_race/main.py
@@ -0,0 +1,10 @@
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet.", prompt="Which turtle will win the race? Enter a color:")
+print(user_bet)
+
+tim = Turtle()
+
+screen.exitonclick()
