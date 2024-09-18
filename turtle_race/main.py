c17807aeeeb095186e11c6cee01b82cae9e8f4df
caroc24 <carolinacamarda@gmail.com>
2024-09-18 13:29:50 -0400

    completed the while loop for the race mechanisms

--- turtle_race/main.py
+++ turtle_race/main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+import random
 
 is_race_on = False
 screen = Screen()
@@ -6,17 +7,29 @@ screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet.", prompt="Which turtle will win the race? Enter a color:")
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 y_pos = [-75, -45, -15, 15, 45, 75]
+all_turtles = []
 
 for i in range(0,6):
-    tim = Turtle(shape="turtle")
-    tim.color(colors[i])
-    tim.penup()
-    tim.goto(x=-230, y=y_pos[i])
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[i])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y_pos[i])
+    all_turtles.append(new_turtle)
 
 # makes sure race doesn't start until user places bet
 if user_bet:
     is_race_on = True
 
 while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if user_bet == winning_color:
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost. The {winning_color} turtle is the winner.")
+        distance = random.randint(0,10)
+        turtle.forward(distance)
 
 screen.exitonclick()
