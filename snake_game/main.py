9fece78adf0ee49e204fc0726499ca0ccc72040b
caroc24 <carolinacamarda@gmail.com>
2024-09-20 13:47:18 -0400

    created wall collision in main

--- snake_game/main.py
+++ snake_game/main.py
@@ -26,9 +26,14 @@ while game_is_on:
     time.sleep(0.1)
 
     snake.move()
+    # Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
         scoreboard.increase_score()
+
+    # Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() > -280 or snake.head.ycor() > 280 or snake.head.ycor() > -280:
+        game_is_on = False
 
 
 screen.exitonclick()
