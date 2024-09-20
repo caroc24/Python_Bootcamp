9d72fa35f797825330cb801362fa753221510415
caroc24 <carolinacamarda@gmail.com>
2024-09-20 14:23:24 -0400

    created extend func

--- snake_game/snake.py
+++ snake_game/snake.py
@@ -25,6 +25,9 @@ class Snake:
         new_segment.goto(position)
         self.segments.append(new_segment)
 
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
