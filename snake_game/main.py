368eb15ae8daec46f9bdd3f637b214ef856a3089
caroc24 <carolinacamarda@gmail.com>
2024-09-20 14:27:05 -0400

    added game over to main

--- snake_game/main.py
+++ snake_game/main.py
@@ -35,6 +35,7 @@ while game_is_on:
     # Detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_is_on = False
+        scoreboard.game_over()
 
 
 screen.exitonclick()
