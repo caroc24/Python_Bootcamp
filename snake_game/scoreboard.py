2201e4522f6222f1865869afe3ef9c5739d514ed
caroc24 <carolinacamarda@gmail.com>
2024-09-20 13:43:41 -0400

    created constant

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -1,5 +1,6 @@
 from turtle import Turtle
-
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
 
 class Scoreboard(Turtle):
 
@@ -13,7 +14,7 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
