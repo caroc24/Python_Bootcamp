49e5182987b06164d6e9f0c93c6ddb1d9ba5bcd8
caroc24 <carolinacamarda@gmail.com>
2024-09-20 13:29:55 -0400

    added modifications to init

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -6,4 +6,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.hideturtle()
+        self.penup()
+        self.color("white")
+        self.goto(x=0, y=270)
         self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
