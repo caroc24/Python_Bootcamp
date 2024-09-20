f00217bee2688da20977ab9a73f6105fac3ed0bf
caroc24 <carolinacamarda@gmail.com>
2024-09-20 07:57:18 -0400

    create_snake() added

--- snake_game/snake.py
+++ snake_game/snake.py
@@ -1,13 +1,18 @@
 from turtle import Turtle
+STARTING_POSITIONS = [(0, 0), (-20,0), (-40,0)]
+
 class Snake:
     """Models the snake that plays the game"""
     def __init__(self):
-        snake_body = []
+        self.segments = []
+        self.create_snake()
+
+    def create_snake(self):
         for i in range(0, 3):
             new_segment = Turtle("square")
             new_segment.color("white")
             new_segment.penup()
-            new_segment.goto(x=i * -20, y=0)
-            snake_body.append(new_segment)
+            new_segment.goto(STARTING_POSITIONS[i])
+            self.segments.append(new_segment)
 
 
