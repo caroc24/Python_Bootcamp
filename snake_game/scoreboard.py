e557a9f6fdb7535e991d11c6d370f9b0e8cc571c
caroc24 <carolinacamarda@gmail.com>
2024-09-20 13:35:48 -0400

    created update_scoreboard func

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -10,7 +10,12 @@ class Scoreboard(Turtle):
         self.penup()
         self.color("white")
         self.goto(x=0, y=270)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
         self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
 
     def increase_score(self):
         self.score += 1
+        self.clear()
+        self.update_scoreboard()
