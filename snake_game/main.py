f533c76b468445d0282303a239b7d17fea7de8c4
caroc24 <carolinacamarda@gmail.com>
2024-09-20 08:00:23 -0400

    move() added

--- snake_game/main.py
+++ snake_game/main.py
@@ -14,10 +14,6 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
 
-    for seg_num in range(len(snake_body) - 1, 0, -1):
-        new_x = snake_body[seg_num - 1].xcor()
-        new_y = snake_body[seg_num - 1].ycor()
-        snake_body[seg_num].goto(new_x, new_y)
-    snake_body[0].forward(20)
+
 
 screen.exitonclick()
