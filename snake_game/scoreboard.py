bfd89066a7d26ce3d52becb357cd5ce752e2b9c2
caroc24 <carolinacamarda@gmail.com>
2024-10-04 09:04:51 -0400

    added highscore from file

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -8,7 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.highscore = 0
+        with open("data.txt") as data:
+            self.highscore = int(data.read())
         self.hideturtle()
         self.penup()
         self.color("white")
@@ -22,6 +23,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.highscore:
             self.highscore = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.highscore}")
         self.score = 0
         self.update_scoreboard()
 
