602b28684e7365cf200311c32833188305a86992
caroc24 <carolinacamarda@gmail.com>
2024-10-04 08:52:55 -0400

    added highscore

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -2,11 +2,13 @@ from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Courier", 24, "normal")
 
+
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.highscore = 0
         self.hideturtle()
         self.penup()
         self.color("white")
@@ -14,12 +16,14 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.highscore}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.color("white")
-        self.goto(0,0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
